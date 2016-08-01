84e139da5deb50212cfe08550b1df860ea340a80
yuny <q165411054@qq.com>
2016-08-01 21:26:00 +0800

    增加对商铺和通用链接的过滤

--- src/js/background.js
+++ src/js/background.js
@@ -19,22 +19,14 @@ chrome.tabs.onUpdated.addListener(function (nTabId, oChangeInfo, oTab) {
 
 function fCheckUrl(sURL) {
     if(sURL){
-        var sHostName = '';
-        var sSearch = /\?/.test(sURL) ? sURL.split('?')[1] : '';
-        var oMatch = sURL.match(/.*:\/\/\/?([^\/]*)/);
-        if(oMatch){
-            sHostName = oMatch[1];
-            if(fCheckDomain(sHostName) && fCheckSubDomain(sHostName) && fCheckSearch(sSearch)){
-                return true
-            }
-        }
+        return fCheckDomain(fGetUrlHostName(sURL));
     }
     return false;
 }
 
 function fCheckDomain(sHostName){
-    for(var cnt = 0, length = SUB_DOMAIN_LIST.length; cnt < length; cnt ++){
-        var oRegExp = new RegExp(SUB_DOMAIN_LIST[cnt]);
+    for(var cnt = 0, length = DOMAIN_LIST.length; cnt < length; cnt ++){
+        var oRegExp = new RegExp(DOMAIN_LIST[cnt]);
         if(oRegExp.test(sHostName)){
             return true;
         }
@@ -42,9 +34,17 @@ function fCheckDomain(sHostName){
     return false;
 }
 
+function fIsShop(sURL){
+    return /\/shop\/view_shop\.htm/.test(sURL);
+}
+
+function fIsGoodsDetail(sURL){
+    return fCheckSubDomain(fGetUrlHostName(sURL)) && fCheckSearch(fGetUrlSearch(sURL));
+}
+
 function fCheckSubDomain(sHostName) {
-    for(var cnt = 0, length = DOMAIN_LIST.length; cnt < length; cnt ++){
-        var oRegExp = new RegExp(DOMAIN_LIST[cnt]);
+    for(var cnt = 0, length = SUB_DOMAIN_LIST.length; cnt < length; cnt ++){
+        var oRegExp = new RegExp(SUB_DOMAIN_LIST[cnt]);
         if(oRegExp.test(sHostName)){
             return true;
         }
@@ -58,17 +58,48 @@ function fCheckSearch(sSearch) {
 
 function fOnUrlMatch(oTab) {
     chrome.pageAction.show(oTab.id);
-    LINKS[oTab.id] = fFilterUrl(oTab.url);
+    var sRawUrl = oTab.url;
+    if(fIsShop(sRawUrl)){
+        LINKS[oTab.id] = fFilterShopUrl(sRawUrl);
+    }else if(fIsGoodsDetail(sRawUrl)){
+        LINKS[oTab.id] = fFilterGoodsUrl(sRawUrl);
+    }else{
+        LINKS[oTab.id] = fFilterCommonUrl(sRawUrl);
+    }
 }
 
 function fOnUrlNotMatch(oTab) {
     chrome.pageAction.hide(oTab.id);
 }
 
-function fFilterUrl(sURL) {
+function fGetUrlHostName(sURL){
+    var oMatch = sURL.match(/.*:\/\/\/?([^\/]*)/);
+    if(oMatch){
+        return oMatch[1];
+    }
+    return '';
+}
+
+function fGetUrlSearch(sURL){
+    return /\?/.test(sURL) ? sURL.split('?')[1] : '';
+}
+
+function fFilterShopUrl(sURL) {
+    return sURL.split('/shop/')[0];
+}
+
+function fFilterGoodsUrl(sURL) {
     var aURLSections = sURL.split('?');
     var sLocation = aURLSections[0];
     var sSearch = aURLSections[1];
     var sIDParam = sSearch.match(/(id=\d+)/);
     return sLocation + '?' + sIDParam[1];
+}
+
+function fFilterCommonUrl(sRawURL) {
+    sRawURL = sRawURL.replace(/[&\?]?spm=[\w\.\-]*/, '');
+    sRawURL = sRawURL.replace(/[&\?]?t=\d*/, '');
+    // 来源个人中心的店铺点击
+    sRawURL = sRawURL.replace(/[&\?]?user_number_id=\d*/, '');
+    return sRawURL;
 }
